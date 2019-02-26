4ec5991adbc072739be8dfc4e8fe58895829db3a
Zahory Velazquez <zv80@humboldt.edu>
2019-02-26 02:10:07 -0800

    finished pa2

--- homework/pa2/py/graph.py
+++ homework/pa2/py/graph.py
@@ -20,6 +20,9 @@ class Graph:
         #tracks known distances
         distances = {}
 
+        # keep track of visited path
+        visited = []
+
         #make sure start is in graph
         if start in self._graph:
 
@@ -32,20 +35,55 @@ class Graph:
 
             #while PQ is not empty
             while len(to_visit) > 0:
-
+       
                 #pop returns item in Python
                 top = heappop(to_visit)
 
                 #2nd item is our key
                 key = top[1]
 
+                addkey = [key]
+
                 if not key in distances:
+                    # have a list of values where first element is shortest time
+                    # and second element is the visited path
+                    visited = visited + addkey
+                    distances[key] = [top[0], visited]
 
                     #record distance
-                    distances[key] = top[0]
 
                     #push on children
                     for edge, weight in self._graph[key].items():
                         if not edge in distances:
                             heappush(to_visit, (weight + top[0], edge))
+
+
+        return distances
+
+# similar to the function above, expect we want to return the list of 
+# keys it took to get to end destination
+# instead of storing the frequencies for distance values,
+#  lets store list of nodes visited
+
+    def buildingsPassed(self, start):
+        # keys are codes, values is list
+        distances = {}
+        # visited keys
+        visited = []
+        
+        if start in self._graph:
+            trackpq = []
+            heappush(trackpq, (0, start))
+            while len(trackpq) > 0:
+                top = heappop(trackpq)
+             
+                key = top[1]
+                addkey = [key]
+                if key not in distances:
+                    visited = visited + addkey
+                    distances[key] = [top[0], visited]
+
+                    for edge, weight in self._graph[key].items():
+                        if not edge in distances:
+                            heappush(trackpq, (weight + top[0], edge))
         return distances
--- homework/pa2/py/newmain.py
+++ homework/pa2/py/newmain.py
@@ -1,3 +1,8 @@
+# Zahory Velazquez
+# id: 012896205
+# PA 2 - program that has user input an HSU location and prints the shortest time
+#        and shortest path
+
 import csv 
 from graph import Graph
 from locations import Locations
@@ -10,32 +15,62 @@ def process_csv(file_name):
             data.append(row)
     return data
 
-hsuMap = Graph()
-loc = Locations()
+def secondsToMin(time_seconds):
+    #show 2 decimal places
+    print("{:.2f}".format(time_seconds/60) + " minutes")
 
-for i in loc._locations:
-    hsuMap.add_vertex(i)
+# look for end key in the computed-short graph
+def searchShortest(key, computed_map):
+    if key in computed_map:
+        return computed_map[key]
+
 
 mapData = process_csv("distances.csv")
+hsuMap = Graph()
+location = Locations()    
+
+for i in location._locations:
+    hsuMap.add_vertex(i)
 
 for items in mapData:
+    # change last element in list to a float (for adding purposes)
+    items[2] = float(items[2])
+
+    # each element in list becomes an argument for paramter
     hsuMap.connect_vertex(*items)
 
-
 print("** HSU Transit Time Calculator **")
+
+print("TIER 1")
 print("Enter starting location: ")
 start = input()
-beg_code = loc.look_up("Cedar")
-hsuMap.compute_shortest_path(beg_code)
+beg_code = location.look_up(start)
+paths = hsuMap.compute_shortest_path(beg_code)
+
+
 print("Enter destination: ")
-#end = input()
-#end_code = loc.look_up("FWH")
+end = input()
+end_code = location.look_up(end)
+
 print("Etimated travel time: ")
-#total = input()
+time = searchShortest(end_code, paths)
+secondsToMin(time[0])
 
+print("Tier 2")
+print("Enter starting location: ")
+begin = input()
+begin_key = location.look_up(begin)
+short_paths = hsuMap.compute_shortest_path(begin_key)
 
+print("Enter destination: ")
+destination = input()
+destination_key = location.look_up(destination)
 
+print("Etimated travel time: ")
+total = searchShortest(destination_key, short_paths)
+secondsToMin(total[0])
 
-
-
-
+print("You will travel to: ") 
+for i in total[1]:
+    # each list represents the locations within the keys traveled
+    print(location._locations[i])
