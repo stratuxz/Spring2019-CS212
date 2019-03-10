48f3a6910109da74a0027c43cfa37bf7072190d7
Zahory Velazquez <zv80@humboldt.edu>
2019-03-09 22:13:45 -0800

    no message

--- homework/pa3/starter code/py/main.py
+++ homework/pa3/starter code/py/main.py
@@ -1,43 +1,47 @@
+#Zahory Velazquez
+#03.09.2019
+
 import csv
 from functions import *
 from graph import Graph
 
 print("*** Tier 1 Route Planner***")
-print("Enter map file: ")
+print("Enter map file: ",end='')
 first_map = input()
 firstCityMap = Graph()
 fileToMap(firstCityMap, first_map)
 
-print("Enter destination file: ")
+print("Enter destination file: ", end='')
 first_dest = input()
 delivery_locs = process_csv(first_dest)
 first_mst  = firstCityMap.compute_minimum_spanning_tree(delivery_locs[0][0])
 
-print("Total Transit Time: ", end= " ")
-print(calculateMstTime(first_mst))
+print("Total Transit Time: ", end= "")
+print(calculateMstTime(first_mst), end='\n\n')
 
 print("*** Tier 2 Route Planner ***")
-print("Enter map file: ")
+print("Enter map file: ", end='')
 second_map = input()
 secondCityMap = Graph()
 fileToMap(secondCityMap, second_map)
 
-print("Enter destination file: ")
+print("Enter destination file: ", end='')
 second_dest = input()
 second_mst = fasterDeliveryPath(secondCityMap, second_dest)
+
 print("Total Transit Time: ", end='')
-print(calculateMstTime(second_mst))
+print(calculateMstTime(second_mst), end='\n\n')
 
 print("*** Tier 3 Route Planner ***")
-print("Enter map file: ")
+print("Enter map file: ", end='')
 third_map = input()
 thirsCityMap = Graph()
 fileToMap(thirsCityMap, third_map)
 
-print("Enter destination file: ")
+print("Enter destination file: ", end='')
 third_dest = input()
 third_mst = fasterDeliveryPath(thirsCityMap, third_dest)
 
-print("Total Transit Time: ", end=' ')
+print("Total Transit Time: ", end='')
 print(calculateMstTime(third_mst))
 printFasterPath(third_mst)
